f6f79d99df722239c3455adff5e8f671ad011869
giotto-learn <maintainers@giotto.ai>
2020-11-06 16:02:31 +0100

    public url for dataset

--- gdeep/heatmaps/__init__.py
+++ gdeep/heatmaps/__init__.py
@@ -4,11 +4,12 @@ from .gradcam import GradCam, minmax_norm, hooked_backward, clamp_gradients_hook
 
 
 __all__ = [
-    'SimpleNN',
-    'DeeperNN',
-    'train_classification_nn',
-    'SaveOutput',
-    'SaveNodeOutput',
-    'SaveLayerOutput',
-    'get_activations'
+    'GradCam',
+    'minmax_norm',
+    'hooked_backward',
+    'clamp_gradients_hook',
+    'hooked_ReLU',
+    'guided_backprop',
+    'get_grad_heatmap',
+    'show_heatmap'
     ]
