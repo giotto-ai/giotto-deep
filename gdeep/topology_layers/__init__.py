d9eb7f961260e62d98cf048adf5606213a337451
Raphael Reinauer <reinauerr@googlemail.com>
2021-08-11 15:47:48 +0000

    refactor code

--- gdeep/topology_layers/__init__.py
+++ gdeep/topology_layers/__init__.py
@@ -1,18 +1,22 @@
 
 from .persformer import SetTransformer,\
-    SelfAttentionSetTransformer
+    SelfAttentionSetTransformer, GraphClassifier
 from .modules import ISAB, PMA, SAB
-from .preprocessing import load_data
+from .preprocessing import load_data, load_augmented_data_as_tensor, load_data_as_tensor, pad_pds
 from .sam import SAM
 from .training import compute_accuracy, train, sam_train
 
 __all__ = [
     'SetTransformer',
     'SelfAttentionSetTransformer',
+    'GraphClassifier',
     'ISAB',
     'PMA',
     'SAB',
     'load_data',
+    'load_augmented_data_as_tensor',
+    'load_data_as_tensor',
+    'pad_pds',
     'SAM',
     'compute_accuracy',
     'train',
