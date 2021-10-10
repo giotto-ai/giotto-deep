f34299ae27b0ea1d81e67ad91fdd612b87929092
giotto-learn <maintainers@giotto.ai>
2021-10-11 00:08:57 +0200

    fixing multi TPUs again

--- gdeep/analysis/decision_boundary/decision_boundary_calculator.py
+++ gdeep/analysis/decision_boundary/decision_boundary_calculator.py
@@ -11,14 +11,6 @@ if torch.cuda.is_available():
 else:
     DEVICE = torch.device("cpu")
 
-try:
-    import torch_xla
-    import torch_xla.core.xla_model as xm
-    DEVICE = xm.xla_device()
-    print("Using TPU!")
-except:
-    print("No TPUs...")
-
 
 class DecisionBoundaryCalculator(ABC):
     """
--- gdeep/models/extractor.py
+++ gdeep/models/extractor.py
@@ -9,14 +9,6 @@ if torch.cuda.is_available():
 else:
     DEVICE = torch.device("cpu")
 
-try:
-    import torch_xla
-    import torch_xla.core.xla_model as xm
-    DEVICE = xm.xla_device()
-    print("Using TPU!")
-except:
-    print("No TPUs...")
-
 class ModelExtractor:
     """This class wraps nn.Modules to extract
     weights, activations, gradients and decision boundaries
