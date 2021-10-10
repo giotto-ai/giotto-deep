1e1c845b67afca521606450a4984489def50e9d1
giotto-learn <maintainers@giotto.ai>
2021-10-11 00:13:20 +0200

    removing TPUS import

--- gdeep/utility/utils.py
+++ gdeep/utility/utils.py
@@ -1,25 +1,13 @@
 import torch
 
-if torch.cuda.is_available():
-    DEVICE = torch.device("cuda")
-else:
-    DEVICE = torch.device("cpu")
-
-try:
-    import torch_xla
-    import torch_xla.core.xla_model as xm
-    DEVICE = xm.xla_device()
-    print("Using TPU!")
-except:
-    print("No TPUs...")
 
 def _are_compatible(model_dict, dataloaders_dict):
     """utility function to check the compatibility of a model
     with a set of dataloaders `(dl_tr, dl_val, dl_ts)`
     """
 
-    model = model_dict["model"].to(DEVICE)
-    batch = next(iter(dataloaders_dict["dataloaders"][0]))[0].to(DEVICE)
+    model = model_dict["model"]
+    batch = next(iter(dataloaders_dict["dataloaders"][0]))[0]
     try:
         model(batch)
     except RuntimeError:
