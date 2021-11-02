bf6a0c367c20adedefbfff5e5eb500849df79dd5
raphaelreinauer <reinauerr@gmail.com>
2021-11-02 13:06:33 +0100

    define data generator class

--- gdeep/data/parallel_orbit.py
+++ gdeep/data/parallel_orbit.py
@@ -1,10 +1,141 @@
-from typing import List
+from typing import List, Tuple
 import numpy as np  # type: ignore
 import torch
 import multiprocessing
 import torch
 from einops import rearrange  # type: ignore
 from gtda.homology import WeakAlphaPersistence  # type: ignore
+
+
+class orbits_generator(object):
+    """Generate
+
+    Args:
+        parameters ([type]): [description]
+        num_classes ([type]): [description]
+        num_orbits_per_class ([type]): number of orbits per class
+        num_pts_per_orbit ([type]): [description]
+        homology_dimensions ([type]): [description]
+        validation_percentage (float, optional): [description]. Defaults to 0.
+        test_percentage (float, optional): [description]. Defaults to 0.
+        dynamical_system (str, optional): either use persistence paths
+            convention ´pp_convention´ or the classical convention
+            ´classical_convention´. Defaults to '´classical_convention´'.
+    """
+    def __init__(self,
+             parameters: Tuple[float] = (2.5, 3.5, 4.0, 4.1, 4.3),
+             num_classes: int = 5,
+             num_orbits_per_class: int = 1_000,
+             num_pts_per_orbit: int = 1_000,
+             homology_dimensions = (0, 1),
+             validation_percentage: float=0,
+             test_percentage: float=0,
+             dynamical_system: str='classical_convention',
+             n_jobs: int=1,
+             ) -> None:
+
+        self.parameters = parameters
+        self.num_classes = num_classes
+        self.num_orbits_per_class = num_orbits_per_class
+        self.num_pts_per_orbit = num_pts_per_orbit
+        self.homology_dimension = homology_dimensions
+        self.validation_percentage = validation_percentage
+        self.test_percentage = test_percentage
+        self.num_homology_dimensions = len(self.homology_dimensions)
+        
+        assert dynamical_system in ('classical_convention', 'pp_convention'),\
+            f'{dynamical_system} is not supported'
+        self.dynamical_system = dynamical_system
+        
+        # Initalize orbits array and persistence diagrams array with None.
+        self.orbits = None
+        self.labels = None
+        self.persistence_diagrams = None
+            
+    def _generate_orbits(self) -> None:
+        if self.orbits != None:
+            return
+        else:
+            x = np.zeros((
+                self.num_classes,  # type: ignore
+                self.num_orbits,
+                self.num_pts_per_orbit,
+                2
+            ))
+
+            y = np.array([self.num_orbits * [c] for c in range(self.num_classes)])
+            
+            self.labels = y.reshape(-1)
+
+            # generate dataset
+            for cidx, p in enumerate(self.parameters):  # type: ignore
+                x[cidx, :, 0, :] = np.random.rand(self.num_orbits, 2)  # type: ignore
+
+                for i in range(1, self.num_pts_per_orbit):  # type: ignore
+                    x_cur = x[cidx, :, i - 1, 0]
+                    y_cur = x[cidx, :, i - 1, 1]
+
+                    if self.dynamical_system == 'pp_convention':
+                        x[cidx, :, i, 0] = (x_cur + p * y_cur * (1. - y_cur)) % 1
+                        x[cidx, :, i, 1] = (y_cur + p * x_cur * (1. - x_cur)) % 1
+                    else:
+                        x[cidx, :, i, 0] = (x_cur + p * y_cur * (1. - y_cur)) % 1
+                        x_next = x[cidx, :, i, 0]
+                        x[cidx, :, i, 1] = (y_cur + p * x_next * (1. - x_next)) % 1
+                        
+            self.orbits = x.reshape((-1, self.num_pts_per_orbit, 2))
+    
+    def _compute_persistence_diagrams(self) -> None:
+        """ Computes the weak alpha persistence of the orbit data clouds.
+
+            The result is stored in member variable ´persistence_diagrams´.
+            It has the shape
+            [num_classes, num_orbits, num_persistence_points, 3].
+            In the last dimension the first two values are the coordinates of
+            the points in the persistence diagrams and the third is the
+            homology dimension.
+        """
+        if self.orbit == None:
+            self._generate_orbits()
+        
+        # compute weak alpha persistence
+        wap = WeakAlphaPersistence(
+                            homology_dimensions=self.homology_dimensions,
+                            n_jobs=self.n_jobs
+                            )
+        # c: class, o: orbit, p: point, d: dimension
+        orbits_stack = rearrange(self.orbits, 'c o p d -> (c o) p d')  # stack classes
+        diagrams = wap.fit_transform(orbits_stack)
+        # shape: (num_classes * n_samples, n_features, 3)
+
+        # combine class and orbit dimensions
+        self.persistence_diagrams = rearrange(
+                                diagrams,
+                                '(c o) p d -> c o p d',
+                                c=num_classes  # type: ignore
+                            )
+    
+    def get_point_cloud(self) -> np.ndarray:
+        if self.orbits == None:
+            self._generate_orbits()
+        return self.orbits
+    
+    def get_persistence_diagrams(self) -> np.ndarray:
+        if self.persistence_diagrams is None:
+            _compute_persistence_diagrams()
+            
+        return self.persistence_diagrams
+    
+    def _split_data(self, validation_size, test_size) -> None:
+        X_train, X_test, y_train, y_test 
+            = train_test_split(, self.labels, test_size=test_size, random_state=1)
+
+        X_train, X_val, y_train, y_val 
+            = train_test_split(X_train, y_train, test_size=, random_state=1)
+    
+    def get_point_cloud_dataset(self):
+        pass
+
 
 def generate_orbit_parallel(
     num_classes,
