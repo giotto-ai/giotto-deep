cfe594ec2e2020a03cc925293c6859802605d89c
matteocao <maintainers@giotto.ai>
2022-02-02 23:19:51 +0100

    make consistent hparams table and optuna studies metrics

--- gdeep/utility/__init__.py
+++ gdeep/utility/__init__.py
@@ -1,10 +1,11 @@
 from .utils import _are_compatible, save_model_and_optimizer, \
-    ensemble_wrapper
+    ensemble_wrapper, _inner_refactor_scalars
 
 __all__ = [
     '_are_compatible',
     'save_model_and_optimizer',
     'optimisation',
     'ensemble_wrapper',
-    'intersection_homology'
+    'intersection_homology',
+    '_inner_refactor_scalars'
     ]
