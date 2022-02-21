aaed3e5c2abbdc46410c896dcfc50ce34a2b49ab
raphaelreinauer <reinauerr@googlemail.com>
2022-02-21 20:05:29 +0100

    finished data_cloud class

--- gdeep/data/data_cloud.py
+++ gdeep/data/data_cloud.py
@@ -1,5 +1,6 @@
 from google.cloud import storage  # type: ignore
 import os
+from typing import Union
 
 class DataCloud():
     def __init__(
@@ -10,8 +11,10 @@ class DataCloud():
         """Download handle for Google Cloud Storage buckets.
 
         Args:
-            bucket_name (str, optional): Name of the Google Cloud Storage bucket. Defaults to "adversarial_attack".
-            download_directory (str, optional): Directory of the downloaded files. Defaults to os.path.join('examples', 'data', 'DataCloud').
+            bucket_name (str, optional): Name of the Google Cloud Storage bucket.
+                Defaults to "adversarial_attack".
+            download_directory (str, optional): Directory of the downloaded files.
+                Defaults to os.path.join('examples', 'data', 'DataCloud').
         """
         self.bucket_name = bucket_name
         self.storage_client = storage.Client()
@@ -28,13 +31,40 @@ class DataCloud():
         
     def download(self,
                  source_blob_name: str,
-                 destination_file_name: str):
+                 destination_file_name: Union[str, None] = None):
         """Download a blob from Google Cloud Storage bucket.
 
         Args:
             source_blob_name (str): Name of the blob to download.
             destination_file_name (str): Name of the destination file to download.
         """
+        if destination_file_name is None:
+            destination_file_name = source_blob_name
         blob = self.bucket.blob(source_blob_name)
         blob.download_to_filename(os.path.join(self.download_directory, destination_file_name))
+        
+    def upload(self,
+               source_file_name: str,
+               destination_blob_name: str,
+               upload_directory: Union[str, None] = None):
+        """Upload a local file to Google Cloud Storage bucket.
+
+        Args:
+            source_file_name (str): Filename of the local file to upload. This
+                has to be in the directory ´upload_directory´.
+            source_blob_name (str): _description_
+            upload_directory (str): Directory where the local file should be. Defaults to "".
+        """
+        if upload_directory is None:
+            upload_directory = ""
+        blob = self.bucket.blob(destination_blob_name)
+        blob.upload_from_filename(os.path.join(upload_directory, source_file_name))
+        
+    def delete_blob(self,
+                    blob_name: str):
+        blob = self.bucket.blob(blob_name)
+        blob.delete()
+        
+        
+        
 
--- gdeep/data/tests/test_data_cloud.py
+++ gdeep/data/tests/test_data_cloud.py
@@ -3,34 +3,86 @@ import os
 import hashlib
 import google
 import logging
+import random
 
 LOGGER = logging.getLogger(__name__)
 import pytest
 
+def credentials_error_logging(func):
+    def inner():
+        try:
+            func()
+        except google.auth.exceptions.DefaultCredentialsError:
+            LOGGER.warning("GCP credentials failed.")
+    return inner
+
+
+def file_as_bytes(file):
+    """Returns a bytes object representing the file
+
+    Args:
+        file (str): File to read.
+
+    Returns:
+        _type_: Byte object
+    """
+    with file:
+        return file.read()
+
+@credentials_error_logging
 def test_download():
     """Test download of sample data from bucket
     """
-    try:
-        data_cloud = DataCloud()
-        file_name = "giotto-deep-big.png"
-        data_cloud.download(file_name,
-                            file_name)
-        
-        # check if correct extension is raised when trying to download non-existing file
-        with pytest.raises(google.api_core.exceptions.NotFound):
-            non_existing_file_name: str = "giotto-deep-bigs.png"
-            data_cloud.download(non_existing_file_name,
-                                non_existing_file_name)
-        
-        # check if downloaded file exists
-        file_path = os.path.join(data_cloud.download_directory, file_name)
-        assert os.path.exists(file_path)
-        
-        def file_as_bytes(file):
-            with file:
-                return file.read()
+    data_cloud = DataCloud()
+    file_name = "giotto-deep-big.png"
+    data_cloud.download(file_name,
+                        file_name)
+    
+    # check if correct extension is raised when trying to download non-existing file
+    with pytest.raises(google.api_core.exceptions.NotFound):
+        non_existing_file_name: str = "giotto-deep-bigs.png"
+        data_cloud.download(non_existing_file_name,
+                            non_existing_file_name)
+    
+    # check if downloaded file exists
+    file_path = os.path.join(data_cloud.download_directory, file_name)
+    assert os.path.exists(file_path)
 
-        # check if downloaded file is correct
-        assert "d4b12b2dc2bc199831ba803431184fcb" == hashlib.md5(file_as_bytes(open(file_path, 'rb'))).hexdigest()
-    except google.auth.exceptions.DefaultCredentialsError:
-        LOGGER.warning("GCP credentials failed.")
+    # check if downloaded file is correct
+    assert "d4b12b2dc2bc199831ba803431184fcb" == \
+        hashlib.md5(file_as_bytes(open(file_path, 'rb'))).hexdigest()
+        
+@credentials_error_logging
+def test_upload():
+    """Test upload of sample file to bucket.
+    """
+    data_cloud = DataCloud()
+    
+    # create temporary file to upload to bucket
+    sample_file_name = "tmp.txt"
+    sample_text = "Create a new tmp file!" + str(random.randint(0, 1_000))
+    
+    if os.path.exists(sample_file_name):
+        os.remove(sample_file_name)
+    with open(sample_file_name, 'w') as f:
+        f.write(sample_text)
+    
+    assert os.path.exists(sample_file_name)
+    
+    # upload sample file to bucket
+    data_cloud.upload(sample_file_name,
+                      sample_file_name)
+    
+    # delete local temporary file
+    os.remove(sample_file_name)
+    
+    data_cloud.download(sample_file_name)
+    
+    data_cloud.delete_blob(sample_file_name)
+    
+    # check if downloaded file exists
+    file_path = os.path.join(data_cloud.download_directory, sample_file_name)
+    assert os.path.exists(file_path)
+    
+    with open(file_path, 'r') as f:
+        assert f.read() == sample_text
