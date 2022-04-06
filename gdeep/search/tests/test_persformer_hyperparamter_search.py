550523c1a382ccb4809087298c1feaaa41c28ebc
raphaelreinauer <reinauerr@googlemail.com>
2022-04-06 21:50:22 +0200

    deleted newly creates files and directories

--- gdeep/search/tests/test_persformer_hyperparamter_search.py
+++ gdeep/search/tests/test_persformer_hyperparamter_search.py
@@ -13,6 +13,12 @@ def test_start():
     download_directory = join('gdeep','search', 'tests', 'data')
     path_hpo_metadata = join('gdeep','search', 'tests', 'data','hpo_metadata.json')
     path_writer = None
+    
+    # Get all directories in the root directory
+    directories = [d for d in listdir(ROOT_DIR) if os.path.isdir(join(ROOT_DIR, d))]
+    
+    # Get all files in the root directory
+    files = [f for f in listdir(ROOT_DIR) if os.path.isfile(join(ROOT_DIR, f))]
 
     hpo = PersformerHyperparameterSearch(dataset_name, download_directory, path_hpo_metadata, path_writer)
 
@@ -22,9 +28,12 @@ def test_start():
     writer_directory = join(ROOT_DIR, 'runs')
     rmtree(writer_directory)
     
-    # Delete all events* and 202* files in the ROOT_DIR
-    for file in listdir(ROOT_DIR):
-        if file.startswith('events') or file.startswith('202'):
-            remove(join(ROOT_DIR, file))
+    # Delete all newly created directories
+    for directory in directories:
+        if directory not in directories:
+            rmtree(join(ROOT_DIR, directory))
 
-    
+    # Delete all newly created files
+    for file in files:
+        if file not in files:
+            remove(join(ROOT_DIR, file))
