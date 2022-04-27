288d446ef73e3f161b24522d19b7da25943ec408
raphaelreinauer <reinauerr@googlemail.com>
2022-04-27 10:54:50 +0200

    fixed small errors in graph dataset

--- gdeep/data/graph_datasets.py
+++ gdeep/data/graph_datasets.py
@@ -61,9 +61,11 @@ class PersistenceDiagramFromGraphDataset(Dataset):
         
         # Load the dataset
         self.graph_dataset = TUDataset(root=DEFAULT_GRAPH_DIR,
-                                    name=dataset_name,
+                                    name=self.dataset_name,
                                     use_node_attr=False,
                                     use_edge_attr=False)
+        
+        labels: List[Tuple[int, int]] = []
         
         for graph_idx, graph in enumerate(self.graph_dataset):
             if graph_idx % 100 == 0:
@@ -80,14 +82,16 @@ class PersistenceDiagramFromGraphDataset(Dataset):
             
             # Save the persistence diagram in a file
             np.save(
-                (os.path.join(output_dir, "diagrams",
+                (os.path.join(self.output_dir, "diagrams",
                               f"graph_{graph_idx}_persistence_diagram.npy")),
                 persistence_diagram_one_hot
                 )
+            
+            labels.append((graph_idx, graph.y.item()))
 
         # Save the labels in a csv file
         np.savetxt(
-            (os.path.join(output_dir, "labels.csv")),
+            (os.path.join(self.output_dir, "labels.csv")),
             labels,
             delimiter=",",
             fmt="%d"
@@ -129,60 +133,57 @@ class PersistenceDiagramFromGraphDataset(Dataset):
         Returns:
             The batch of persistence diagrams as a tensor.
         """
-        # Convert the batch to a tensor
-        batch_tensor = torch.tensor(batch)
-        
-        # Return the batch as a tuple
-        return batch_tensor, torch.tensor(self.labels)
+        # TODO: Implement this method
+        pass
     
     
-dataset_name = "REDDIT-BINARY"
-diffusion_parameter: float = 10.0
+# dataset_name = "REDDIT-BINARY"
+# diffusion_parameter: float = 10.0
 
-# Check if DEFAULT_GRAPH_DIR exists
-if not os.path.exists(DEFAULT_GRAPH_DIR):
-    raise ValueError("The directory {} does not exist!")
+# # Check if DEFAULT_GRAPH_DIR exists
+# if not os.path.exists(DEFAULT_GRAPH_DIR):
+#     raise ValueError("The directory {} does not exist!")
 
-# Load the dataset
-graph_dataset = TUDataset(root=DEFAULT_GRAPH_DIR,
-                 name=dataset_name,
-                 use_node_attr=False,
-                 use_edge_attr=False)
+# # Load the dataset
+# graph_dataset = TUDataset(root=DEFAULT_GRAPH_DIR,
+#                  name=dataset_name,
+#                  use_node_attr=False,
+#                  use_edge_attr=False)
 
-# Compute the heat kernel signature and the extended persistence of all the
-# graphs in the dataset and save them in a file
+# # Compute the heat kernel signature and the extended persistence of all the
+# # graphs in the dataset and save them in a file
 
-# Define directory where to save the results
-output_dir = os.path.join(DEFAULT_GRAPH_DIR, dataset_name + 
-                          "_extended_persistence")
-# Create the directory if it does not exist
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
-    os.makedirs(os.path.join(output_dir, "diagrams"))
-else:
-    raise ValueError("Output directory already exists!")
+# # Define directory where to save the results
+# output_dir = os.path.join(DEFAULT_GRAPH_DIR, dataset_name + 
+#                           "_extended_persistence")
+# # Create the directory if it does not exist
+# if not os.path.exists(output_dir):
+#     os.makedirs(output_dir)
+#     os.makedirs(os.path.join(output_dir, "diagrams"))
+# else:
+#     raise ValueError("Output directory already exists!")
 
 
-labels: List[Tuple[int, int]] = []
+# labels: List[Tuple[int, int]] = []
 
-for graph_idx, graph in enumerate(graph_dataset):
-    if graph_idx % 100 == 0:
-        print(f"Processing graph {graph_idx}")
+# for graph_idx, graph in enumerate(graph_dataset):
+#     if graph_idx % 100 == 0:
+#         print(f"Processing graph {graph_idx}")
     
-    # Get the adjacency matrix
-    adj_mat: np.ndarray = to_dense_adj(graph.edge_index)[0].numpy()
+#     # Get the adjacency matrix
+#     adj_mat: np.ndarray = to_dense_adj(graph.edge_index)[0].numpy()
     
-    # Compute the extended persistence
-    persistence_diagram_one_hot = \
-        graph_extended_persistence_hks(adj_mat, 
-                                       diffusion_parameter=diffusion_parameter)
+#     # Compute the extended persistence
+#     persistence_diagram_one_hot = \
+#         graph_extended_persistence_hks(adj_mat, 
+#                                        diffusion_parameter=diffusion_parameter)
     
 
-    # Save the persistence diagram in a file
-    np.save(
-        (os.path.join(output_dir, "diagrams",
-                      f"graph_{graph_idx}_persistence_diagram.npy")),
-        persistence_diagram_one_hot
-        )
-    labels.append((graph_idx, graph.y.item()))
+#     # Save the persistence diagram in a file
+#     np.save(
+#         (os.path.join(output_dir, "diagrams",
+#                       f"graph_{graph_idx}_persistence_diagram.npy")),
+#         persistence_diagram_one_hot
+#         )
+#     labels.append((graph_idx, graph.y.item()))
     
