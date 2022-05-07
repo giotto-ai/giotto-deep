ba85adced1be8089ab371223812bdc53ef098073
raphaelreinauer <reinauerr@googlemail.com>
2022-05-08 00:48:39 +0200

    added PersistenceDiagramFeatureExtractor

---
+++ gdeep/utility/constants.py
@@ -0,0 +1,19 @@
+import os
+
+# Define the root directory of the project which is parent of the parent of 
+# the current directory
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                        os.pardir, os.pardir))
+
+# Define the default dataset download directory
+DEFAULT_DOWNLOAD_DIR = os.path.join(ROOT_DIR, "examples", "data",
+                                    "DatasetCloud")
+
+# Define the default dataset bucket on Google Cloud Storage where the datasets
+# are stored
+DATASET_BUCKET_NAME = "adversarial_attack"
+
+
+# Define the default dataset download directory where the graph
+# datasets from the PyG (PyTorch Geometric) library are stored
+DEFAULT_GRAPH_DIR = os.path.join(ROOT_DIR, "examples", "data", "GraphDatasets")
