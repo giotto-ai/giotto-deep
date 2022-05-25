fixed normalizing pd

examples/mutag_pipeline.py
@@ -1,14 +1,24 @@
 # %%
 import os
 from shutil import rmtree
-from typing import List
+from typing import List, Tuple
 from sklearn.model_selection import train_test_split
 
 import numpy as np
+import torch
 from torch.utils.data import DataLoader, Subset
 
+from gdeep.data.datasets import PersistenceDiagramFromFiles
 from gdeep.data.datasets.persistence_diagrams_from_graphs_builder import PersistenceDiagramFromGraphBuilder
 from gdeep.data.persistence_diagrams.one_hot_persistence_diagram import OneHotEncodedPersistenceDiagram
+from gdeep.data import PreprocessingPipeline
+from gdeep.data.preprocessors import (
+    NormalizationPersistenceDiagram,
+    FilterPersistenceDiagramByLifetime,
+    FilterPersistenceDiagramByHomologyDimension
+)
+from gdeep.data.preprocessors.normalization import _compute_mean_of_dataset
+from gdeep.data.transforming_dataset import TransformingDataset
 from gdeep.utility.utils import autoreload_if_notebook
 from gdeep.utility import DEFAULT_GRAPH_DIR
 
@@ -23,26 +33,32 @@ num_homology_types: int = 4
 # Create the persistence diagram dataset
 pd_creator = PersistenceDiagramFromGraphBuilder(name_graph_dataset, diffusion_parameter)
 pd_creator.create()
-
 # %%
-# Print sample extended persistence diagram
+# Plot sample extended persistence diagram
 file_path: str = os.path.join(DEFAULT_GRAPH_DIR,
                               f"MUTAG_{diffusion_parameter}_extended_persistence", "diagrams")
-graph_idx = 4
+graph_idx = 1
 pd: OneHotEncodedPersistenceDiagram = \
     OneHotEncodedPersistenceDiagram.load(os.path.join(file_path, 
-                                                      f"graph_{graph_idx}_persistence_diagram.npy"))
+                                                      f"{graph_idx}.npy"))
 names = ["Ord0", "Ext0", "Rel1", "Ext1"]
 pd.plot(names)
 # %%
 
-pd_mutag_ds: Dataset[Tuple[OneHotEncodedPersistenceDiagram, int]] = \
-    PersistenceDiagramFromFiles(name_graph_dataset, 0.1)
+pd_mutag_ds = PersistenceDiagramFromFiles(
+    os.path.join(
+        DEFAULT_GRAPH_DIR, f"MUTAG_{diffusion_parameter}_extended_persistence"
+        )
+)
 
 pd: OneHotEncodedPersistenceDiagram = pd_mutag_ds[0][0]
 
-pd.plot()
-
+fig = pd.plot(["Ord0", "Ext0", "Rel1", "Ext1"])
+# add title
+fig.show()
+# %%
+pd - torch.tensor([1])
+# %%
 
 # Create the train/validation/test split
 
@@ -52,7 +68,7 @@ train_indices, test_indices = train_test_split(
     random_state=42,
 )
 
-train_indices: List[int], validation_indices: List[int] = train_test_split(
+train_indices , validation_indices = train_test_split(
     train_indices,
     test_size=0.2,
     random_state=42,
@@ -64,19 +80,36 @@ validation_dataset = Subset(pd_mutag_ds, validation_indices)
 test_dataset = Subset(pd_mutag_ds, test_indices)
 
 # Preprocess the data
-preprocessing_pipeline = PreprocessingPipeline(
-    (ToTensorImage((32, 32)), Normalization()))
+preprocessing_pipeline = PreprocessingPipeline[Tuple[OneHotEncodedPersistenceDiagram, int]](
+    (
+        FilterPersistenceDiagramByHomologyDimension[int]([0, 1]),
+        FilterPersistenceDiagramByLifetime[int](min_lifetime=-0.1, max_lifetime=1.0),
+        NormalizationPersistenceDiagram[int](num_homology_dimensions=4),
+     )
 )
 
 preprocessing_pipeline.fit_to_dataset(train_dataset)
+
+# %%
 
 train_dataset = preprocessing_pipeline.attach_transform_to_dataset(train_dataset)
 validation_dataset = preprocessing_pipeline.attach_transform_to_dataset(validation_dataset)
 test_dataset = preprocessing_pipeline.attach_transform_to_dataset(test_dataset)
 
+# %%
+_compute_mean_of_dataset(
+            TransformingDataset(train_dataset, lambda x: (x[0].mean(dim=0), x[1]))
+            )
+# -> (0.0, 0.0, sth, sth)
+
+_compute_mean_of_dataset(
+            TransformingDataset(train_dataset, lambda x: ((x[0]**2).mean(dim=0), x[1]))
+            )
+# -> (1.0, 1.0, sth, sth)
+# %%
 
 # Build the data loaders
-dl_builder = DataLoaderBuilder(train_dataset, validation_dataset, test_dataset)
+dl_builder = DataLoaderBuilder(train_dataset, validation_dataset, test_dataset, collate_fn)
 dl_train ... = dl_builder.build
 
 # Define the model
@@ -105,4 +138,16 @@ x = np.random.rand(10, 2)
 # compute pairwise distances
 dists = np.linalg.norm(x[:, np.newaxis, :] - x[np.newaxis, :, :], axis=-1)
 
- 
+
+# %%
+Tensor = torch.Tensor
+
+class A(Tensor):
+    pass        
+class B(Tensor):
+    pass
+x = torch.tensor([1.0, 2.0, 3.0])
+a = A(x)
+b = B(x)
+type(a + b)
+# %%

gdeep/data/datasets/persistence_diagrams_from_graphs_builder.py
@@ -106,8 +106,6 @@ class PersistenceDiagramFromGraphBuilder:
             
             # Get the adjacency matrix
             adj_mat: np.ndarray = to_dense_adj(graph.edge_index)[0].numpy()
-            if(graph_idx == 1):
-                print(adj_mat)
             
             # Compute the extended persistence
             persistence_diagram_one_hot = \
@@ -120,7 +118,7 @@ class PersistenceDiagramFromGraphBuilder:
             # Save the persistence diagram in a file
             persistence_diagram_one_hot.save(
                 os.path.join(self.output_dir, "diagrams",
-                              f"graph_{graph_idx}_persistence_diagram.npy")
+                              f"{graph_idx}.npy")
             )
             
             # Save the label

gdeep/data/persistence_diagrams/one_hot_persistence_diagram.py
@@ -13,19 +13,20 @@ Array = np.ndarray
 class OneHotEncodedPersistenceDiagram(Tensor):
     """This class represents a single one-hot encoded persistence diagram.
     """
+    raw_data: Tensor
     def __init__(self, data: Tensor):  # type: ignore
         super().__init__()
         _check_if_valid(data)
         self.data = _sort_by_lifetime(data)
     
-    def __add__(self, other: Any) -> Tensor:
-        raise ValueError("The addition of persistence diagrams is not supported.")
+    # def __add__(self, other: Any) -> Tensor:
+    #     raise ValueError("The addition of persistence diagrams is not supported.")
     
-    def __sub__(self, other: Any) -> Tensor:
-        raise ValueError("The subtraction of persistence diagrams is not supported.")
+    # def __sub__(self, other: Any) -> Tensor:
+    #     raise ValueError("The subtraction of persistence diagrams is not supported.")
     
-    def __div__(self, other: Any) -> Tensor:
-        return super().__div__(other)
+    # def __div__(self, other: Any) -> Tensor:
+    #     return super().__div__(other)
     
     def get_num_homology_dimensions(self) -> int:
         """This method returns the number of homology dimensions.
@@ -101,6 +102,17 @@ class OneHotEncodedPersistenceDiagram(Tensor):
                             names=names)
         
         return fig
+    
+    def all_close(self, other: 'OneHotEncodedPersistenceDiagram',
+                 atol: float) -> bool:
+        """This method checks if the persistence diagrams are close.
+        """
+        for i in range(self.get_num_homology_dimensions()):
+            if not torch.allclose(self.get_all_points_in_homology_dimension(i),
+                                    other.get_all_points_in_homology_dimension(i),
+                                    atol=atol):
+                return False
+        return True
         
     def get_lifetimes(self) -> Tensor:
         """This method returns the lifetimes of the points.

gdeep/data/preprocessors/normalization.py
@@ -1,8 +1,11 @@
 import warnings
 from typing import TypeVar, Tuple, Any
+from matplotlib.pyplot import axis
 
 import torch
 from torch.utils.data import Dataset
+
+from gdeep.data.persistence_diagrams.one_hot_persistence_diagram import OneHotEncodedPersistenceDiagram
 
 from ..abstract_preprocessing import AbstractPreprocessing
 from ..transforming_dataset import TransformingDataset
@@ -10,12 +13,13 @@ from ..transforming_dataset import TransformingDataset
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 Tensor = torch.Tensor
+PD = OneHotEncodedPersistenceDiagram
 
 T = TypeVar('T')
 
 class Normalization(AbstractPreprocessing[Tuple[Tensor, T], Tuple[Tensor, T]]):
     """This class runs the standard normalisation on all the dimensions of
-    the tensors of a dataloader. For example, in case of images where each item is of
+    the tensors of a dataset. For example, in case of images where each item is of
     shape ``(C, H, W)``, the average will and the standard deviations
     will be tensors of shape ``(C, H, W)``
     """